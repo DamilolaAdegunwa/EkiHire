1d20156b80e2bf4857f4ab1678227bd52a992207
Damilola Adegunwa <adegunwad@accessbankplc.com>
2021-05-02 15:49:43 +0100

    commit

---
+++ EKIHIRE/EkiHire.Data/Migrations/20210502144141_userimg_adpValue2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EkiHire.Data.Migrations
+{
+    public partial class userimg_adpValue2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
